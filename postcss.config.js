49127d39e909194bbde385afb6b6c4c658460ab4
tangmouren <1678977488@qq.com>
2022-08-01 12:01:58 +0800

    feat: 增加 vw 移动端适配方案

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: {
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px',
+      viewportWidth: 750, // 设计稿的宽度
+      unitPrecision: 5, // 单位转换后保留的精度
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [], // 不需要转换的
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: undefined,
+      include: undefined,
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 1460,
+    },
+  },
 };
